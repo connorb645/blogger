b4dab0e190cde56e7018b895831b2bece5517c33
Connor Black <connor.b645@gmail.com>
2021-10-11 21:01:56 +0100

    Added new project

---
+++ blogger/Shared/Helpers/Networking.swift
@@ -0,0 +1,8 @@
+//
+//  Networking.swift
+//  blogger
+//
+//  Created by Connor Black on 11/10/2021.
+//
+
+import Foundation
---
+++ blogger/Shared/bloggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  bloggerApp.swift
+//  Shared
+//
+//  Created by Connor Black on 11/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct bloggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
